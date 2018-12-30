6ba42ce60f0447d1d444693391fc317b53e99c32
Timm <timm.rasmussen@gmail.com>
2018-12-30 20:11:52 +0100

    Add project files.

---
+++ ExpressApp1/app.js
@@ -0,0 +1,104 @@
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+var jwt = require('./_helpers/jwt');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+
+        if (typeof (err) === 'string') {
+            // custom application error
+            return res.status(400).json({ message: err });
+        }
+
+        if (err.name === 'ValidationError') {
+            // mongoose validation error
+            return res.status(400).json({ message: err.message });
+        }
+
+        if (err.name === 'UnauthorizedError') {
+            // jwt authentication error
+            return res.status(401).json({ message: 'Invalid Token' });
+        }
+
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+
+    if (typeof (err) === 'string') {
+        // custom application error
+        return res.status(400).json({ message: err });
+    }
+
+    if (err.name === 'ValidationError') {
+        // mongoose validation error
+        return res.status(400).json({ message: err.message });
+    }
+
+    if (err.name === 'UnauthorizedError') {
+        // jwt authentication error
+        return res.status(401).json({ message: 'Invalid Token' });
+    }
+
+    // default to 500 server error
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+
+var server = app.listen(port, function () {
+    debug('Express server listening on port ' + server.address().port);
+});
---
+++ ExpressApp1/models/mongo.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/demoDb', function (err) {
+
+    if (err) throw err;
+
+    console.log('Successfully connected');
+
+});
+
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    User: require('./users/user.model')
+};
