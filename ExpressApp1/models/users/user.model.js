42fe37baa3600ffc6d9f080e570cb9b28ed82443
TimmosQuadros <timm.rasmussen@gmail.com>
2019-01-06 21:57:23 +0100

    Added email to user model and email validator.

--- ExpressApp1/models/users/user.model.js
+++ ExpressApp1/models/users/user.model.js
@@ -1,11 +1,20 @@
 ﻿const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator');
+
+/**
+ * A Validation function for local strategy email
+ */
+var validateEmail = function (email) {
+    return validator.isEmail(email, { require_tld: false });
+};
 
 const schema = new Schema({
     username: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    email: { type: String, lowercase: true, trim: true, default: '', validate: [validateEmail, 'Please fill a valid email address']},
     createdDate: { type: Date, default: Date.now }
 });
 
